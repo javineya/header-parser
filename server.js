7044b672a8b3e702d0c1c5c68b4690bb74ace0b0
Glitch (almond-coffee) <none>
2018-11-26 03:40:46 +0000

    🐀🐬 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  var language = req.acceptsLanguages();
+  var software = req.get('User-Agent');
+  var ipAddress = req.ip;
+  res.json({
+    'ipAddress': ipAddress,
+    'language': language,
+    'software': software
+  });
+});
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
